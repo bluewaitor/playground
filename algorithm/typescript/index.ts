7cee5b213c2eb5d480ef782930ef7c8b679a3617
bluewaitor <405622394@qq.com>
2019-06-24 23:31:04 +0800

    feat(algorithm): 冒泡排序。

--- algorithm/typescript/index.ts
+++ algorithm/typescript/index.ts
@@ -1,5 +1,11 @@
 import { LinkedList } from './linked-list/LinkedList';
+import { BubbleSort } from './sorting/BubbleSort';
 
 const list = new LinkedList();
 
 console.log(list.append(1).append(2).append(3).append(4).append(3).append(2).toString());
+
+const bubbleSort = new BubbleSort();
+const array = [3, 4, 9, 0, 1, 3, 5];
+bubbleSort.sort(array);
+console.log(array);
