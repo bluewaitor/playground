504b8ed600efe740505df43e00cf1235070835de
blue <405622394@qq.com>
2020-06-10 10:09:04 +0800

    feat(python): imdb

---
+++ python/deep_learning_with_python/3.4_imdb/simple.py
@@ -0,0 +1,33 @@
+from keras.datasets import imdb
+import numpy as np
+from keras import models
+from keras import layers
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+# 只训练四轮
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
+
+print(results)
+
+print(model.predict(x_test))
