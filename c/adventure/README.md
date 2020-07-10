1531b81f8711ae6406b173dcf86a1c7f01db879e
blue <405622394@qq.com>
2020-07-10 15:58:07 +0800

    feat(c): adventure

---
+++ c/adventure/README.md
@@ -0,0 +1,3 @@
+# C语言文字游戏
+
+[地址](https://home.hccnet.nl/r.helderman/adventures/htpataic01.html)
---
+++ c/adventure/misc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "object.h"
+#include "misc.h"
+
+OBJECT *parseObject(const char *noun) {
+  OBJECT *obj, *found = NULL;
+  for (obj = objs; obj < endOfObjs; obj++)
+  {
+    if (noun != NULL && strcmp(noun, obj->tag) == 0) {
+      found = obj;
+    }
+  }
+
+  return found;
+}
+
+OBJECT *personHere(void) {
+  OBJECT *obj;
+  
+  for (obj = objs; obj < endOfObjs; obj++) {
+    if (obj->location == player->location && obj == guard) {
+      return obj;
+    }
+  }
+
+  return NULL;
+}
+
+int listObjectsAtLocation(OBJECT *location) {
+  int count = 0;
+  OBJECT *obj;
+  for (obj = objs; obj < endOfObjs; obj ++) {
+    if (obj != player && obj->location == location) {
+      if (count++ == 0) {
+        printf("You see: \n");
+      }
+      printf("%s\n", obj-> description);
+    }
+  }
+
+  return count;
+}
