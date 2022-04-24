fd5c47bdf4e640fb191fc27fb53a77e7abefc97a
blue <405622394@qq.com>
2022-04-24 18:06:16 +0800

    feat(typescript): 测试选项

--- typescript/tsconfig/index.ts
+++ typescript/tsconfig/index.ts
@@ -1,7 +1,10 @@
 export { Person } from './Person';
+import print from './src/index';
 
 function printMessage() {
   console.log("hello world");
 }
 
 printMessage();
+
+print();
---
+++ typescript/tsconfig/src/index.ts
@@ -0,0 +1,5 @@
+console.log('hello world');
+
+export default function printMessage() {
+    console.log('hello world');
+}
